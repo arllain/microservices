3a29000959b308dada943a6f036c92f276496b7d
Arllain <arllain@gmail.com>
2021-11-03 07:32:18 -0300

    Created License model, controller, service. Added internationalization

---
+++ licensing-service/src/main/java/com/arllain/licensing/controller/LicenseController.java
@@ -0,0 +1,54 @@
+package com.arllain.licensing.controller;
+
+import com.arllain.licensing.model.License;
+import com.arllain.licensing.service.LicenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
+
+@RestController
+@RequestMapping(value = "v1/organization/{organizationId}/license")
+public class LicenseController {
+
+    private final LicenseService licenseService;
+
+    @Autowired
+    public LicenseController(LicenseService licenseService) {
+        this.licenseService = licenseService;
+    }
+
+    @GetMapping(value = "/{licenseId}")
+    public ResponseEntity<License> getLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId) {
+
+        License license = licenseService.getLicense(licenseId, organizationId);
+        return  ResponseEntity.ok(license);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId, locale));
+    }
+
+    @PostMapping
+    public  ResponseEntity<String> createLicense(
+            @PathVariable("organizationId") String organizationId,
+            @RequestBody License request,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
+    }
+
+    @DeleteMapping(value = "/{licenseId}")
+    public ResponseEntity<String> deleteLicense(
+            @PathVariable("organizationId") String organizationId,
+            @PathVariable("licenseId") String licenseId,
+            @RequestHeader(value = "Accept-Language", required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId, locale));
+    }
+}
